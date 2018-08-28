f377332a5cb63f28c6bccdba2bbdcdc770fa88d7
Aryadne Guardieiro <aryadneccomp@gmail.com>
2018-08-28 12:23:23 -0300

    ajusts at the chrony config file

--- raspberrypi-configuration-files/chrony_config.sh
+++ raspberrypi-configuration-files/chrony_config.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+# to run on a remote host:
+# scp chrony_config.sh USER@IP:~ && sshpass -p "password" ssh USER@IP sh chrony_config.sh
+
 sudo apt install -y chrony && \
 
-sudo sed -i s/pool\ 2.debian.pool.ntp.org\ iburst/#\ pool\ 2.debian.pool.ntp.org\ iburst/g /etc/chrony/chrony.conf && \
+sudo sed -i s/pool\ 2.debian.pool.ntp.org/#\ pool\ 2.debian.pool.ntp.org/g /etc/chrony/chrony.conf && \
 
 sudo sed -i s/makestep\ 1\ 3/makestep\ 1\ -1/g /etc/chrony/chrony.conf && \
 
@@ -12,6 +15,6 @@ echo "server 192.168.0.1 iburst minpoll 2 maxpoll 4" | sudo tee -a /etc/chrony/c
 
 sudo systemctl restart chrony && \
 
-sleep 2
+sleep 5 && \
 
 chronyc sources
