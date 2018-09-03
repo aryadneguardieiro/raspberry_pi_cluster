2c7d4f9cf4baabc53789031474d8f3afbef0f8e6
Aryadne Guardieiro <aryadneccomp@gmail.com>
2018-09-03 17:51:06 -0300

    colecting each metric in a separated file

--- prometheus-files/cvs_export.py
+++ prometheus-files/cvs_export.py
@@ -16,36 +16,41 @@ def GetMetrixNames(url):
 """
 Prometheus hourly data as csv.
 """
-with open('metrics.csv', 'w') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
 
-    if len(sys.argv) != 3:
-        print('Usage: {0} http://localhost:9090 interval(1m,2h,...)'.format(sys.argv[0]))
-        sys.exit(1)
+if len(sys.argv) != 3:
+    print('Usage: {0} http://localhost:9090 interval(1m,2h,...)'.format(sys.argv[0]))
+    sys.exit(1)
 
-    metrixNames=GetMetrixNames(sys.argv[1])
-    interval=sys.argv[2]
-    writeHeader=True
+metrixNames=GetMetrixNames(sys.argv[1])
+interval=sys.argv[2]
+writeHeader=True
 
-    for metrixName in metrixNames:
-        #now its hardcoded for hourly
+for metrixName in metrixNames[0..1]:
+    with open(metrixName + '.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        
         response = requests.get('{0}/api/v1/query'.format(sys.argv[1]), params={'query': metrixName+'['+interval+']'})
         results = response.json()['data']['result']
+        
         # Build a list of all labelnames used.
         #gets all keys and discard __name__
         labelnames = set()
 
         for result in results:
             labelnames.update(result['metric'].keys())
+
         # Canonicalize
         labelnames.discard('__name__')
         labelnames = sorted(labelnames)
+
         # Write the samples.
-        if writeHeader:
-             writer.writerow(['name', 'timestamp', 'value'] + labelnames)
-             writeHeader=False
+        writer.writerow(['name', 'timestamp', 'value'] + labelnames)
+
         for result in results:
-            l = [result['metric'].get('__name__', '')] + result['values']
-            for label in labelnames:
-                l.append(result['metric'].get(label, ''))
+            for value in results['values']:
+                l = [result['metric'].get('__name__', '')]
+                l.append(value[0])
+                l.append(value[1])
+                for label in labelnames:
+                    l.append(result['metric'].get(label, ''))
                 writer.writerow(l)
