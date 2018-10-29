2eb4da95826cd3fe78e6d04d4683be72136b0fd5
Gustavo <guto_silveira_@hotmail.com>
2018-10-29 17:34:43 -0300

    agora sim

--- prometheus-files/extract_csv_from_prometheus.py
+++ prometheus-files/extract_csv_from_prometheus.py
@@ -33,76 +33,92 @@ def create_dir(path):
         print("Error at the creation of directory")
         sys.exit(1)
 
-def get_hour_in_minutes(end_hour_test):
-    hour = int(end_hour_test.split(':')[0])
+def get_hour_in_minutes(begin_hour_test):
+    hour = int(begin_hour_test.split(':')[0])
     return ((24 - hour) * 60)
 
-def get_minute(end_hour_test):
-    return (int(end_hour_test.split(':')[1]))
+def get_minute(begin_hour_test):
+    return (int(begin_hour_test.split(':')[1]))
 
 """
 Prometheus hourly data as csv.
 """
 
 if len(sys.argv) != 6:
-    print('Usage: {0} http://localhost:30000 interval(1m,2h,...) dir_path end_test_day (dd/mm/aa) end_test_hour (hh:mm:ss)'.format(sys.argv[0]))
+    print('Usage: {0} http://localhost:30000 duration(1m,2h,...) dir_path begin_test_day (dd/mm/aa) begin_test_hour (hh:mm:ss) step'.format(sys.argv[0]))
     sys.exit(1)
 
 metrixNames=GetMetrixNames(sys.argv[1])
 interval=sys.argv[2]
+interval_int = int(sys.argv[2][:-1])
 path=sys.argv[3]
-end_hour_test=sys.argv[5]
+begin_hour_test=sys.argv[5]
+step=int(sys.argv[6])
 create_dir(path)
 s = requests.Session()
-offset_til_midnight = get_hour_in_minutes(end_hour_test) - get_minute(end_hour_test)
-end_test_date = datetime.strptime(sys.argv[4]+ ' ' + sys.argv[5], "%d/%m/%y %H:%M:%S")
+offset_til_midnight = get_hour_in_minutes(begin_hour_test) - get_minute(begin_hour_test)
+begin_test_date = datetime.strptime(sys.argv[4]+ ' ' + sys.argv[5], "%d/%m/%y %H:%M:%S")
 current = 0
 total = len(metrixNames)
+
+if interval_int % step != 0:
+    print('Step nao divide o intervalo.')
+    return 1
 
 for metrixName in metrixNames:
     if '_bucket' not in metrixName and '_sum' not in metrixName and '_count' not in metrixName:
         with open(path + '/' + metrixName + '.csv', 'w') as csvfile:
-            now = datetime.utcnow()
-            #offset = str(offset_til_midnight + (now.hour * 60) + now.minute + days_shift * 3600) + "m";
-            time_delta = datetime.now() - end_test_date
-            offset = time_delta.days*1440 + math.ceil(time_delta.seconds/60)
-            offset = str(offset)+'m'
+            
             writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+            
             try:
-                response = s.get('{0}/api/v1/query'.format(sys.argv[1]), params={'query': metrixName+'['+interval+'] offset ' + offset}, timeout=60)
-                results = response.json()['data']['result']
                 labelnames = set()
-
-                for result in results:
-                    labelnames.update(result['metric'].keys())
-
-                # Canonicalize
-                labelnames.discard('__name__')
-                labelnames = sorted(labelnames)
-
-                # Write the samples.
-                writer.writerow(['name'] + labelnames + ['timestamp', 'value'])
-
                 time_series = {}
+                start = begin_teste_date
+                end_test_date = start + timedelta(seconds=(interval_int * 3600))
+                step_in_seconds = (interval_int / step) * 3600
+                shift=timedelta(seconds=step_in_seconds)
+                for current_step in range(1, step + 1):
+                    
+                    end = start + shift
+                    if end > end_test_date:
+                        end = end_test_date
 
-                for result in results:
-                    for value in result['values']:
-                        l = [result['metric'].get('__name__', '')]
-                        tag = result['metric'].get('__name__', '')
+                    response = s.get('{0}/api/v1/query_range'.format(sys.argv[1]), params={'query': metrixName, 'start': start.isoformat() + '.000Z', 'end': end.isoformat() + '.000Z', 'step': '1s'}, timeout=60)
+                    results = response.json()['data']['result']
 
-                        for label in labelnames:
-                            l.append(result['metric'].get(label, ''))
-                            tag = tag +'_'+ result['metric'].get(label, '')
-                        l.append(value[0])
-                        l.append(value[1])
-                        writer.writerow(l)
+                    if current_step == 1:
+                        for result in results:
+                            labelnames.update(result['metric'].keys())
 
-                        if tag in time_series:
-                           if time_series[tag]['x'][-1] != value[0] and time_series[tag]['y'][-1] != value[1]:
-                               time_series[tag]['x'] = time_series[tag]['x'] + [value[0]]
-                               time_series[tag]['y'] = time_series[tag]['y'] + [value[1]]
-                        else:
-                            time_series[tag] = {'x': [value[0]], 'y': [value[1]]}
+                        # Canonicalize
+                        labelnames.discard('__name__')
+                        labelnames = sorted(labelnames)
+
+                        # Write the samples.
+                        writer.writerow(['name'] + labelnames + ['timestamp', 'value'])
+
+                    for result in results:
+                        for value in result['values']:
+                            l = [result['metric'].get('__name__', '')]
+                            tag = result['metric'].get('__name__', '')
+
+                            for label in labelnames:
+                                l.append(result['metric'].get(label, ''))
+                                tag = tag +'_'+ result['metric'].get(label, '')
+                            l.append(value[0])
+                            l.append(value[1])
+                            writer.writerow(l)
+
+                            if tag in time_series:
+                               if time_series[tag]['x'][-1] != value[0] and time_series[tag]['y'][-1] != value[1]:
+                                   time_series[tag]['x'] = time_series[tag]['x'] + [value[0]]
+                                   time_series[tag]['y'] = time_series[tag]['y'] + [value[1]]
+                            else:
+                                time_series[tag] = {'x': [value[0]], 'y': [value[1]]}
+
+                    start = end + timedelta(seconds=1)
+                
                 current = current + 1
                 percentage = current/total * 100
                 print(str(math.ceil(percentage)) + "% arquivos gerados (" +str(current) + "/" + str(total) + ")", end='\r')
