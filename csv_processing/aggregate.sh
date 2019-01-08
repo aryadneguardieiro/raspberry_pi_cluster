6b445214cf1aeaf692f85a170ca456560b9d763f
Gustavo <guto_silveira_@hotmail.com>
2019-01-08 16:44:06 -0200

    atualizacao no script

--- csv_processing/aggregate.sh
+++ csv_processing/aggregate.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-qtdFiles=$(ls | grep csv | wc -l);
+qtdFiles=$(ls | grep csv$ | wc -l);
 headers="timestamp";
-for t in $(ls | grep csv | sort); do
-	headers=$(echo $headers,$(echo $t | cut -d '.' -f 1));
+for t in $(ls | grep csv$ | sed s/\.csv//g | sort); do
+	headers="$headers,$t";
 done
 echo $headers;
 
-file_name=$(ls | grep csv | head -1);
+file_name=$(ls | grep csv$ | head -1);
 
 for t in $(tail -n +2 $file_name); do 
 	x=$(echo $t | cut -d ',' -f 1);
-	qtd=$(cat $(ls | grep csv) | grep $x | wc -l);
+	qtd=$(cat $(ls | grep csv$) | grep $x | wc -l);
 	if [ $qtd -eq $qtdFiles ]; then
-		line=$(echo $(cat $(ls | grep csv | sort) | grep $x) | sed s/$x,//g);
+		line=$(echo $(cat $(ls | grep csv$ | sort) | grep $x) | sed s/$x,//g);
 		echo $x,"${line// /,}";
 	fi
 done;
