73feaaee6c8110e82cb5e44d5a02c73046d7113e
Gustavo <guto_silveira_@hotmail.com>
2019-05-22 16:34:12 -0300

    Adicionando nome na metrica separadamente no arquivo de hash

--- csv_processing/extract_csv_from_prometheus_cassandra.py
+++ csv_processing/extract_csv_from_prometheus_cassandra.py
@@ -46,7 +46,7 @@ def main():
   
   with open(str(map_file_name), 'w') as time_series_map:
     writer_file_map = csv.writer(time_series_map, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-    writer_file_map.writerow(['hash', 'metric_info'])
+    writer_file_map.writerow(['hash', 'metric_name', 'metric_info'])
     for metric_name in metric_names:
       if metric_name not in metrics_to_get:
         continue
@@ -78,7 +78,7 @@ def main():
               hash_object = hashlib.sha1(metric_info.encode())
               time_serie_hash_id = hash_object.hexdigest()
               
-              writer_file_map.writerow([time_serie_hash_id, metric_info])
+              writer_file_map.writerow([time_serie_hash_id, metric_name, metric_info])
               
               file_name = time_serie_hash_id + '.csv'
               file_name = data_folder / file_name
